dfa2d53810ef34f9660d1fabd4e47c878a3d5627
mdsarwarzahan <md.sarwar.zahan@gmail.com>
2015-08-31 01:56:08 +0600

    Added user functionality and security

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * Security controller.
+ *
+ * @Route("/")
+ */
+class SecurityController extends Controller
+{
+    /**
+     * Login form.
+     *
+     * @Route("/login", name="login")
+     * @Method("GET")
+     * @Template()
+     */
+    public function loginAction(Request $request)
+    {
+        $session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                SecurityContextInterface::AUTHENTICATION_ERROR
+            );
+        } elseif (null !== $session && $session->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $session->get(SecurityContextInterface::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContextInterface::AUTHENTICATION_ERROR);
+        } else {
+            $error = '';
+        }
+
+        return $this->render(
+            'AppBundle:Security:login.html.twig',
+            array(
+                'error'         => $error,
+            )
+        );
+    }
+}
